66b4b8e03ff0e3e28e58c091f905efc746b57298
Dasha Semerenko <dasha_semerenko@intuit.com>
2022-07-25 22:04:25 +1000

    playerDetails added

--- CraftWebUI/Data/PlayerApiConsumer.cs
+++ CraftWebUI/Data/PlayerApiConsumer.cs
@@ -21,11 +21,23 @@ namespace CraftWebUI.Data
 		public async Task<PlayerModel[]> GetPlayersAsync()
 		{
 			var apiUrl = this.configuration["PlayersApiUrl"];
+			return await this.GetFromApiAsync<PlayerModel[]>(apiUrl);
+		}
+
+		public async Task<PlayerModel> GetPlayerDetailsAsync(string playerId)
+		{
+			var apiUrl = this.configuration["PlayersApiUrl"];
+			var playerUrl = $"{apiUrl}/{playerId}";
+			return await this.GetFromApiAsync<PlayerModel>(playerUrl);
+		}
+
+		private async Task<T> GetFromApiAsync<T>(string url)
+		{
 			var client = new HttpClient();
-			var response = await client.GetAsync(apiUrl);
+			var response = await client.GetAsync(url);
 			var responseAsString = await response.Content.ReadAsStringAsync();
-			var players = JsonSerializer.Deserialize<PlayerModel[]>(responseAsString, jsonOptions);
-			return players;
+			var data = JsonSerializer.Deserialize<T>(responseAsString, jsonOptions);
+			return data;
 		}
 	}
 }
